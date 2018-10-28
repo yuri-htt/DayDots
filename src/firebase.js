1b1dc9777b16dc3c56611ec39818456d59448c1f
Yuri <yuri.tayama@linksports.co.jp>
2018-10-28 12:39:21 +0900

    Android端末でアプリを実行できるよう修正

--- src/firebase.js
+++ src/firebase.js
@@ -4,13 +4,13 @@ import * as firebase from 'firebase';
 import 'firebase/firestore';
 
 // symbol polyfills
-// global.Symbol = require('core-js/es6/symbol');
-// require('core-js/fn/symbol/iterator');
+global.Symbol = require('core-js/es6/symbol');
+require('core-js/fn/symbol/iterator');
 
 // collection fn polyfills
-// require('core-js/fn/map');
-// require('core-js/fn/set');
-// require('core-js/fn/array/find');
+require('core-js/fn/map');
+require('core-js/fn/set');
+require('core-js/fn/array/find');
 
 class Firebase {
   constructor(config = {}) {
